9a049a6bbd785d1d26d01c6094338245fb59994a
kenrick <kenrickdony@gmail.com>
2022-11-25 09:47:35 +0300

    added functions for arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+let random=Math.floor(Math.random())+1
+let num3= 5
+let num4= 19
+
+let mod= num4 % num3
+console.log(mod)
+
+let max = Math.max
+console.log[max =(1,5,12,16,20)]
+
